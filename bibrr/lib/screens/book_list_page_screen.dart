de6e77e9cb56470cf2cbfc70f6e75c4e43ac7b21
RichelleLippensPXL <richelle.lippens@student.pxl.be>
2024-10-19 12:24:25 +0200

    list page oneidig laden opgelost en error voor scrolleble opgelost

--- bibrr/lib/screens/book_list_page_screen.dart
+++ bibrr/lib/screens/book_list_page_screen.dart
@@ -2,6 +2,7 @@ import 'package:bibrr/data/book.dart';
 import 'package:bibrr/data/http_helper.dart';
 import 'package:bibrr/screens/book_detail_page_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Booklistpagescreen extends StatefulWidget {
   const Booklistpagescreen({super.key});
@@ -14,7 +15,9 @@ class _BooklistpagescreenState extends State<Booklistpagescreen> {
   List<Book> books = [];
   bool isLoading = false;
   final TextEditingController _searchController = TextEditingController();
-  
+  Book? selectedBook; 
+
+
   @override
   void initState() {
     super.initState();
@@ -24,6 +27,7 @@ class _BooklistpagescreenState extends State<Booklistpagescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true, 
       appBar: AppBar(
         title: const Text('BibRR'),
         actions: [
@@ -35,113 +39,121 @@ class _BooklistpagescreenState extends State<Booklistpagescreen> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _searchController,
-                      decoration: InputDecoration(
-                        hintText: 'Search for a book title',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+   
+
+
+        body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          hintText: 'Search for a book title',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: () {
-                      // Initiates a search based on the query entered
-                      getData(query: _searchController.text);
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: isLoading
-                  ? const Center(child: CircularProgressIndicator())
-                  : ListView.builder(
-                      itemCount: books.length,
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: (){
-                            Navigator.push(
-                              context, 
-                              MaterialPageRoute(builder: (context) => Bookdetailpagescreen(book: books[index])),
-                              );
-                          },
-                        
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            border: Border(
-                              top: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 206, 205, 205),
-                                  width: 1),
-                              bottom: BorderSide(
-                                  color:
-                                      const Color.fromARGB(255, 206, 205, 205),
-                                  width: 1),
-                              left: BorderSide(
-                                  color: Colors.transparent,
-                                  width: 1), // Transparent left border
-                              right: BorderSide(
-                                  color: Colors.transparent, width: 1),
-                            ),
-                          ),
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 3, horizontal: 15),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0), // Add Padding for consistency
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                books[index].image.isNotEmpty
-                                    ? Image.network(
-                                        books[index].image,
-                                        width: 80,
-                                        height: 100,
-                                        fit: BoxFit.cover, // Make image fill its space
-                                      )
-                                    : const Icon(Icons.book, size: 80),
-                                const SizedBox(width: 10),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        books[index].title,
-                                        style: const TextStyle(
-                                            fontSize: 16, fontWeight: FontWeight.bold),
-                                      ),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        books[index].author,
-                                        style: const TextStyle(fontSize: 14),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        );
+                    IconButton(
+                      icon: const Icon(Icons.search),
+                      onPressed: () {
+                        // Initiates a search based on the query entered
+                        getData(query: _searchController.text);
                       },
                     ),
-            ),
-          ],
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        itemCount: books.length,
+                        itemBuilder: (context, index) {
+                          
+                          return InkWell(
+                            onTap: (){
+                              Navigator.push(
+                                context, 
+                                MaterialPageRoute(builder: (context) => Bookdetailpagescreen(book: books[index])),
+                                );
+                            },
+                          
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border(
+                                top: BorderSide(
+                                    color:
+                                        const Color.fromARGB(255, 206, 205, 205),
+                                    width: 1),
+                                bottom: BorderSide(
+                                    color:
+                                        const Color.fromARGB(255, 206, 205, 205),
+                                    width: 1),
+                                left: BorderSide(
+                                    color: Colors.transparent,
+                                    width: 1), // Transparent left border
+                                right: BorderSide(
+                                    color: Colors.transparent, width: 1),
+                              ),
+                            ),
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 3, horizontal: 15),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0), // Add Padding for consistency
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  books[index].image.isNotEmpty
+                                      ? Image.network(
+                                          books[index].image,
+                                          width: 80,
+                                          height: 100,
+                                          fit: BoxFit.cover, // Make image fill its space
+                                        )
+                                      : const Icon(Icons.book, size: 80),
+                                  const SizedBox(width: 10),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          books[index].title,
+                                          style: const TextStyle(
+                                              fontSize: 16, fontWeight: FontWeight.bold),
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          books[index].author,
+                                          style: const TextStyle(fontSize: 14),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
         ),
-      ),
+        ),
+
     );
   }
 
@@ -149,6 +161,9 @@ class _BooklistpagescreenState extends State<Booklistpagescreen> {
     setState(() {
       isLoading = true;
     });
+    if (query.isEmpty){
+      query = ':title';
+    }
 
     HttpHelper helper = HttpHelper();
     //List<Book> result = await helper.getBooks();
@@ -160,4 +175,7 @@ class _BooklistpagescreenState extends State<Booklistpagescreen> {
       isLoading = false;
     });
   }
+
+  
+
 }
