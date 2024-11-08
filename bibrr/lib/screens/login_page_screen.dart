8796ad592f3e8b1f069ed2e703aba568c7ff2edd
RuneIndestegePXL <rune.indestege@student.pxl.be>
2024-11-08 11:17:26 +0100

    changed LoginPageScreen  to StatefulWidget, with error message

--- bibrr/lib/screens/login_page_screen.dart
+++ bibrr/lib/screens/login_page_screen.dart
@@ -1,26 +1,44 @@
+// login_page_screen.dart
+
 import 'package:bibrr/services/LanguageService.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
-class LoginPageScreen extends StatelessWidget {
+class LoginPageScreen extends StatefulWidget {
+  const LoginPageScreen({Key? key}) : super(key: key);
 
-  LoginPageScreen({super.key});
+  @override
+  _LoginPageScreenState createState() => _LoginPageScreenState();
+}
 
+class _LoginPageScreenState extends State<LoginPageScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  String _errorMessage = '';
+
   void signUserIn() async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-      email: _emailController.text,
-      password: _passwordController.text,
-    );
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = context.read<LanguageService>().getString(
+          'login_error',
+          'Login failed. Please try again.',
+        );
+      });
+    }
   }
 
   Future<UserCredential> signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
@@ -31,81 +49,95 @@ class LoginPageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final languageService = Provider.of<LanguageService>(context);
+
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false, // Removes the back arrow
         title: Text(languageService.getString('login_title', 'Login')),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: languageService.getString('email_label', 'Email'),
-                border: const OutlineInputBorder(),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // Centers vertically
+            children: [
+              if (_errorMessage.isNotEmpty)
+                Text(
+                  _errorMessage,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText:
+                      languageService.getString('email_label', 'Email'),
+                  border: const OutlineInputBorder(),
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: languageService.getString('password_label', 'Password'),
-                border: const OutlineInputBorder(),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText:
+                      languageService.getString('password_label', 'Password'),
+                  border: const OutlineInputBorder(),
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                signUserIn();
-              },
-              child: Text(languageService.getString('login_button', 'Login')),
-            ),
-            const SizedBox(height: 16.0),
-            Row(
-              children: [
-                const Expanded(child: Divider()),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text(
-                    languageService.getString('or', 'OR'),
-                    style: const TextStyle(color: Colors.grey),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: signUserIn,
+                child: Text(
+                  languageService.getString('login_button', 'Login'),
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                children: [
+                  const Expanded(child: Divider()),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      languageService.getString('or', 'OR'),
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  const Expanded(child: Divider()),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              GestureDetector(
+                onTap: signInWithGoogle,
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/google_logo.png',
+                          height: 24, width: 24),
+                      const SizedBox(width: 10),
+                      Text(
+                        languageService.getString(
+                            'google_sign_in', 'Sign in with Google'),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const Expanded(child: Divider()),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            GestureDetector(
-              onTap: () {
-                signInWithGoogle();
-              },
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/google_logo.png', height: 24, width: 24),
-                    const SizedBox(width: 10),
-                    Text(
-                      languageService.getString(
-                          'google_sign_in', 'Sign in with Google'),
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black54),
-                    ),
-                  ],
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
